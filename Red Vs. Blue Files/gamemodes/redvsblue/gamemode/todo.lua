af0875943e7479f5704a3097259e710219b7cfee
Rudy Raveendran <rudytheninja@gmail.com>
2013-09-16 23:01:35 -0400

    v0.0.03 ToDo and neat folder system

---
+++ Red Vs. Blue Files/gamemodes/redvsblue/gamemode/todo.lua
@@ -0,0 +1,5 @@
+//All the folders you see here are blank, and are unnecessary for first release.
+//They are simply what needs to be accomplished in time.
+//Priority wise, work on scoreboard, hud, classes, teams, and menus first.
+//Classes and teams are just there to help keep the files smaller and neater.
+//The rest of the folders can be populated any time, though objectives, hitmarkers, and shields would give it a halo feel.
