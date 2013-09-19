9d310e5830e9484b40a205d00d8f1d0905e08a8b
Rudy Raveendran <rudytheninja@gmail.com>
2013-09-19 17:47:01 -0400

    V0.0.05 Added Steam IDs of people
    IDs to use for gamemode recognizing people when they join. I'll add this
    in.

--- Red Vs. Blue Files/gamemodes/redvsblue/gamemode/todo.lua
+++ Red Vs. Blue Files/gamemodes/redvsblue/gamemode/todo.lua
@@ -3,3 +3,12 @@
 //Priority wise, work on scoreboard, hud, classes, teams, and menus first.
 //Classes and teams are just there to help keep the files smaller and neater.
 //The rest of the folders can be populated any time, though objectives, hitmarkers, and shields would give it a halo feel.
+/*This is a list of people who have signed up to be perma members ie recognized by game:
+Hagane: STEAM_0:0:51046670 Blue
+gameraider: STEAM_0:0:66885172 Blue
+jimmie_rustler (ask who this is): STEAM_0:0:45590513 blue
+clock face: STEAM_0:0:46291837 red
+
+note must get more.
+*/
+
